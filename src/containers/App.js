4cd40fb3d0747cd10f8af583034defa2e6c8b155
Tim Geurts <geurts.tim@gmail.com>
2016-05-24 22:12:39 +0100

    QOL change temp menu items

--- src/containers/App.js
+++ src/containers/App.js
@@ -19,9 +19,16 @@ export default class CoreLayout extends React.Component {
     return (
       <div>
         <ul className="nav navbar-nav navbar-right">
-          <li><Link className="navbar-brand" to="/">Root</Link></li>
-          <li><Link to="/protected">Protected Content</Link></li>
-          <li><Link to="/login">Login</Link></li>
+          <li><Link className="navbar-brand" to="/">Home</Link></li>
+          <li><Link to="/users">Users</Link></li>
+          { !this.props.isAuthenticated
+           ? <li><Link to="/login">Login</Link></li>
+           : ''
+          }
+          { !this.props.isAuthenticated
+           ? <li><Link to="/register">Register</Link></li>
+           : ''
+          }
           { this.props.isAuthenticated
            ? <li><button onClick={() => this.props.dispatch(logoutAndRedirect())}>Logout</button></li>
            : ''
